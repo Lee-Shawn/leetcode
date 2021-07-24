0b292ba7621e7050e5b6e6542d4dd0a4afd2bb01
ximingze <425984393@qq.com>
2021-07-24 13:50:32 +0800

    验证二叉搜索树

---
+++ 98.验证二叉搜索树.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=java
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode pre;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 访问左子树
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        // 前一个节点要小于后一个节点
+        if (pre != null && pre.val >= root.val) {
+            return false;
+        }
+        pre = root;
+        // 访问右子树
+        if (!isValidBST(root.right)) {
+            return false;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
