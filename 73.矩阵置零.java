84b80c4ffb67cd43c55297bc2bdde08cbccb4a18
ximingze <425984393@qq.com>
2021-07-31 16:09:29 +0800

    矩阵置零

---
+++ 73.矩阵置零.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=73 lang=java
+ *
+ * [73] 矩阵置零
+ */
+
+// @lc code=start
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // 标记每一行是否有0
+        boolean[] row = new boolean[m];
+        // 标记每一列是否有0
+        boolean[] col = new boolean[n];
+        // 统计所有的0
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        // 标记对应的行列为0
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+// @lc code=end
+
