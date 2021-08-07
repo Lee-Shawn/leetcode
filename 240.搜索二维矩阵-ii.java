107fc5a2bb25c8cb5ebecaab9eaac228a045636e
ximingze <425984393@qq.com>
2021-08-07 14:14:21 +0800

    搜索二维矩阵

--- 240.搜索二维矩阵-ii.java
+++ 240.搜索二维矩阵-ii.java
@@ -8,13 +8,24 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         // 从左下角开始寻找
-        for (int i = matrix.length - 1; i >= 0; i--) {
+        /*for (int i = matrix.length - 1; i >= 0; i--) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (target == matrix[i][j]) {
                     return true;
                 } else if (target < matrix[i][j]) {
                     break;
                 }
+            }
+        }*/
+        int row = matrix.length - 1;
+        int col = 0;
+        while (row >= 0 && col < matrix[0].length) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target > matrix[row][col]) {
+                col++;
+            } else if (target < matrix[row][col]) {
+                row--;
             }
         }
         return false;
