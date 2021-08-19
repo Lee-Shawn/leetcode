5049c89dbb6aa8df33a84ecb8083dd4fa9d55d8d
ximingze <425984393@qq.com>
2021-08-19 19:11:02 +0800

    斐波那契数列

---
+++ 509.斐波那契数.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=java
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+class Solution {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        int fib = 0;
+        for (int i = 2; i <= n; i++) {
+            fib = a + b;
+            a = b;
+            b = fib;
+        }
+        return fib;
+    }
+}
+// @lc code=end
+
