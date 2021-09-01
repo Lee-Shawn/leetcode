ac848be1a1ef484023baa3a6bf1ea7e3f763af51
ximingze <425984393@qq.com>
2021-09-01 19:33:59 +0800

    奇偶链表

---
+++ 328.奇偶链表.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=java
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        // 链表为空或者只有一个节点
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (odd.next != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
+// @lc code=end
+
