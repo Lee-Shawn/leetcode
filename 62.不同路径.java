25f9f3af1ca96fabcf84618c533c5c95800ca552
ximingze <425984393@qq.com>
2021-09-02 19:09:03 +0800

    不同路径

---
+++ 62.不同路径.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=java
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        // dp[0][j] 和 dp[i][0] 在边界，只有向右或者向下移动，所以都为1
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                // 状态转移方程
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+// @lc code=end
+
