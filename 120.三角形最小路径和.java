2e88bd96793706742a2b4a6ae72261318463d566
ximingze <425984393@qq.com>
2021-09-04 16:23:57 +0800

    三角形最小路径和

---
+++ 120.三角形最小路径和.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=120 lang=java
+ *
+ * [120] 三角形最小路径和
+ */
+
+// @lc code=start
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int length = triangle.size();
+        int[] dp = new int[length + 1];
+        // 从下往上遍历
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+}
+// @lc code=end
+
