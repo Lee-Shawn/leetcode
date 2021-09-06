2d08c9ea54d1fd0192e77351935ffd0f7f1c059c
ximingze <425984393@qq.com>
2021-09-06 22:03:19 +0800

    最大正方形

---
+++ 221.最大正方形.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=221 lang=java
+ *
+ * [221] 最大正方形
+ */
+
+// @lc code=start
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    // 如果matrix[i][j] = 0，则dp[i][j]=0，因为它无法构成正方形
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        // dp(i,j)=min(dp(i−1,j),dp(i−1,j−1),dp(i,j−1))+1
+                        dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+}
+// @lc code=end
+
