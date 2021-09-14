682836c5b7aa41ab123d89831d560514d75dffb0
ximingze <425984393@qq.com>
2021-09-14 23:15:13 +0800

    94. 二叉树的中序遍历

---
+++ 94.二叉树的中序遍历.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        inorder(list, root);
+        return list;
+    }
+
+    private void inorder(List<Integer> list, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(list, root.left);
+        list.add(root.val);
+        inorder(list, root.right);
+    }
+}
+// @lc code=end
+
