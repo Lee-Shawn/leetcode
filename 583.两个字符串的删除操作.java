418bab5dc2fbeb9f42be396277121ffe664d7788
ximingze <425984393@qq.com>
2021-09-25 11:06:11 +0800

    两个字符串的删除操作

---
+++ 583.两个字符串的删除操作.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=583 lang=java
+ *
+ * [583] 两个字符串的删除操作
+ */
+
+// @lc code=start
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return m - dp[m][n] + n - dp[m][n];
+    }
+}
+// @lc code=end
+
