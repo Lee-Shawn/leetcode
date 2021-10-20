4f22dd18fa65db8f482bc2dfbe0d58717ff4b545
ximingze <425984393@qq.com>
2021-10-20 21:51:07 +0800

    最小操作使数组元素全部相等

---
+++ 453.最小操作次数使数组元素相等.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=453 lang=java
+ *
+ * [453] 最小操作次数使数组元素相等
+ */
+
+// @lc code=start
+class Solution {
+    public int minMoves(int[] nums) {
+        int n = nums.length;
+        long min = nums[0], sum = 0;
+        for (int i : nums) {
+            min = Math.min(min, i);
+            sum += i;
+        }
+        return (int)(sum - min * n);
+    }
+}
+// @lc code=end
+
