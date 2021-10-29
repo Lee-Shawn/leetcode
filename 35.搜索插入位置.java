9da6a29f58d31f2692aa56ee81883a19f02281fd
ximingze <425984393@qq.com>
2021-10-29 23:59:45 +0800

    搜索插入位置

---
+++ 35.搜索插入位置.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int result = nums.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target <= nums[mid]) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
